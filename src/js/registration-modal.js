4eb473b82cf38125869e23dcf9d36d50a31b55a0
Alejogvy <Byui15.>
2025-02-02 22:29:51 -0500

    commit

--- src/js/registration-modal.js
+++ src/js/registration-modal.js
@@ -3,15 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const closeModal = document.getElementById("close-modal");
     const registerButton = document.getElementById("register-button");
 
-    // Show the modal every time the page loads
     modal.style.display = "flex";
 
-    // Close the modal when clicking "Close"
     closeModal.addEventListener("click", () => {
         modal.style.display = "none";
     });
 
-    // Redirect to registration page when clicking "Register"
     registerButton.addEventListener("click", () => {
         window.location.href = "register.html";
     });
